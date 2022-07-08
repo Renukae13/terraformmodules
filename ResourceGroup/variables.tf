e38378c3cdd9f7fa367fff7e9f1cb9544338b934
Renukae13 <103033903+Renukae13@users.noreply.github.com>
2022-07-08 22:15:06 +0530

    Update variables.tf

--- ResourceGroup/variables.tf
+++ ResourceGroup/variables.tf
@@ -1,8 +1,8 @@
-  variable "resource_group_name_1" {
+  variable "resource_group_name" {
     type = string
-    default = "test1"
+    default = ""
   }
   variable "resource_location" {
     type = string
-    default = "West Europe"
+    default = ""
   }
