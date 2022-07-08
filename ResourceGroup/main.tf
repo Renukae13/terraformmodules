a14eb657fec22b035f2b63181414ecc452de84b7
Renukae13 <103033903+Renukae13@users.noreply.github.com>
2022-07-08 21:02:07 +0530

    Update main.tf

--- ResourceGroup/main.tf
+++ ResourceGroup/main.tf
@@ -1,7 +1,3 @@
-data "azurerm_resource_group" "main" {
-    name = var.resource_group_name_1
-  
-}
 resource "azurerm_resource_group" "demo1" {
   name     = data.azurerm_resource_group.main
   location = var.resource_location
